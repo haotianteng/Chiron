31a8fb3d054f6b045c2e51d6255d7881963a9931
Haotian Teng <havens.teng@gmail.com>
2017-08-09 01:15:34 +1000

    remove ctcbns folder

--- ctcbns/run_cpu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sh /shares/coin/haotian.teng/deepBNS/ctcbns/ctcbns/sub_bash_cpu.sh "sh delta_run_ctcbns_cpu.sh" 16 "120G" 100 "crnn3+3_S10"
--- ctcbns/run_local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source activate tensorflow
-export PYTHONPATH=$PYTHONPATH:/home/haotianteng/UQ/deepBNS/
-python ctcbns_rcnn_train.py
