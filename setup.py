2a368ba3ab129434427e63c973cff169e554fc9b
Haotian Teng <havens.teng@gmail.com>
2017-08-22 23:41:05 +1000

    setup fixed.

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 setup(
   name = 'chiron',
   packages = ['chiron'], # this must be the same as the name above
-  version = '0.1.2.2',
+  version = '0.1.2.3',
   include_package_data=True,
   description = 'A deep neural network basecaller for nanopore sequencing.',
   author = 'Haotian Teng',
   author_email = 'havens.teng@gmail.com',
   url = 'https://github.com/haotianteng/chiron', 
-  download_url = 'https://github.com/haotianteng/chiron/archive/0.1.2.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/haotianteng/chiron/archive/0.1.2.3.tar.gz', # I'll explain this in a second
   keywords = ['basecaller', 'nanopore', 'sequencing','neural network'], # arbitrary keywords
   license="MPL 2.0",
   classifiers = ['License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)'],
