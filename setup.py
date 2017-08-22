0f6b506eccadfc1d789170697b05711f4b0dee0b
Haotian Teng <havens.teng@gmail.com>
2017-08-22 17:34:52 +1000

    h5py dependency added

--- setup.py
+++ setup.py
@@ -12,16 +12,16 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 setup(
   name = 'chiron',
   packages = ['chiron'], # this must be the same as the name above
-  version = '0.1.13',
+  version = '0.1.2.1',
   include_package_data=True,
   description = 'A deep neural network basecaller for nanopore sequencing.',
   author = 'Haotian Teng',
   author_email = 'havens.teng@gmail.com',
   url = 'https://github.com/haotianteng/chiron', 
-  download_url = 'https://github.com/haotianteng/chiron/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/haotianteng/chiron/archive/0.1.2.1.tar.gz', # I'll explain this in a second
   keywords = ['basecaller', 'nanopore', 'sequencing','neural network'], # arbitrary keywords
   license="MPL 2.0",
   classifiers = ['License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)'],
-  install_requires=['tensorflow==1.0.1'],
+  install_requires=['tensorflow-gpu==1.0.1','h5py==2.6.0'],
   entry_points={'console_scripts':['chiron=chiron.entry:main'],}
 )
