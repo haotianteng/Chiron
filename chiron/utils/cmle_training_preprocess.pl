ba5b4bb5f9bd9198e461563cc38889909c59ac63
Allen Day <allenday@allenday.com>
2017-09-22 13:10:01 -0700

    preserve original base filename for factorized training

--- chiron/utils/cmle_training_preprocess.pl
+++ chiron/utils/cmle_training_preprocess.pl
@@ -1,19 +1,18 @@
 #!/usr/bin/perl
 use strict;
 my $base = shift @ARGV;
-
-if ( ! $base ) {
-  print STDERR "Usage: $0 [input prefix]\nwhere appending prefix with (.signal,.label) are readable paths\n";
+my $out  = shift @ARGV;
+if ( ! $base || ! $out ) {
+  print STDERR "Usage: $0 <input path prefix> <output path>\nwhere appending prefix with (.signal,.label) are readable paths\n";
   exit(1);
 }
-
 open( SIGNAL, "$base.signal" ) or die "Can't open .signal file: $!";
-open( LABEL,  "$base.label" )  or die "Can't open .label  file: $!";
+open( LABEL,  "$base.label"  ) or die "Can't open .label  file: $!";
+open( OUT,    ">$out"        ) or die "Can't write to '$out':   $!";
 my @signal = split /\s+/, <SIGNAL>;
-
 while ( my $line = <LABEL> ) {
   chomp $line;
   my ( $start, $end, $label ) = split /\s+/, $line;
   my @slice = @signal[ $start .. ($end-1) ];
-  print $label, "\t", join( ",", @slice ), "\n";
+  print OUT $label, "\t", join( ",", @slice ), "\t", $base, "\n";
 }
