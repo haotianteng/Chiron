0d47bae8ebe31c9076e6b8f8d3eccc67fb24a551
mbhall88 <mbhall88@gmail.com>
2018-02-17 21:19:01 +0000

    python-modernize and fix spelling mistake in read_raw_data_sets passed parameters.

--- chiron/chiron_cnn_train.py
+++ chiron/chiron_cnn_train.py
@@ -5,10 +5,13 @@ Created on Mon Apr 17 17:32:32 2017
 
 @author: haotianteng
 """
+from __future__ import absolute_import
+from __future__ import print_function
 import tensorflow as tf
 
-from chiron_input import read_raw_data_sets
-from cnn import getcnnlogit
+from .chiron_input import read_raw_data_sets
+from .cnn import getcnnlogit
+from six.moves import range
 
 
 class Flags():
@@ -29,7 +32,9 @@ def inference(x):
 
 
 def loss(logits, seq_len, label):
-    return tf.reduce_mean(tf.nn.ctc_loss(label, logits, seq_len, ctc_merge_repeated=False, time_major=False))
+    return tf.reduce_mean(
+        tf.nn.ctc_loss(label, logits, seq_len, ctc_merge_repeated=False,
+                       time_major=False))
 
 
 def train_step(loss):
@@ -39,13 +44,19 @@ def train_step(loss):
 
 def prediction(logits, seq_length, label):
     logits = tf.transpose(logits, perm=[1, 0, 2])
-    predict = tf.to_int32(tf.nn.ctc_beam_search_decoder(logits, seq_length, merge_repeated=False)[0][0])
-    error = tf.reduce_sum(tf.edit_distance(predict, label, normalize=False)) / tf.to_float(tf.size(label.values))
+    predict = tf.to_int32(
+        tf.nn.ctc_beam_search_decoder(logits, seq_length, merge_repeated=False)[
+            0][0])
+    error = tf.reduce_sum(
+        tf.edit_distance(predict, label, normalize=False)) / tf.to_float(
+        tf.size(label.values))
     return error
 
 
 def train():
-    train_ds, valid_ds = read_raw_data_sets(FLAGS.data_dir, FLAGS.sequence_len, valid_reads_num=10000)
+    train_ds, valid_ds = read_raw_data_sets(FLAGS.data_dir,
+                                            seq_length=FLAGS.sequence_len,
+                                            max_reads_num=10000)
     x = tf.placeholder(tf.float32, shape=[FLAGS.batch_size, FLAGS.sequence_len])
     seq_length = tf.placeholder(tf.int32, shape=[FLAGS.batch_size])
     y_indexs = tf.placeholder(tf.int64)
@@ -68,14 +79,19 @@ def train():
             batch_x, seq_len, batch_y = train_ds.next_batch(FLAGS.batch_size)
             indxs, values, shape = batch_y
             loss_val, _ = sess.run([ctc_loss, opt],
-                                   feed_dict={x: batch_x, seq_length: seq_len, y_indexs: indxs, y_values: values,
+                                   feed_dict={x: batch_x, seq_length: seq_len,
+                                              y_indexs: indxs, y_values: values,
                                               y_shape: shape})
             if i % 10 == 0:
                 error_val = sess.run(error,
-                                     feed_dict={x: batch_x, seq_length: seq_len, y_indexs: indxs, y_values: values,
+                                     feed_dict={x: batch_x, seq_length: seq_len,
+                                                y_indexs: indxs,
+                                                y_values: values,
                                                 y_shape: shape})
-                print "Epoch %d, batch number %d, loss: %5.2f edit_distance: %5.2f" \
-                      % (train_ds.epochs_completed, train_ds.index_in_epoch, loss_val, error_val)
+                print(
+                    "Epoch %d, batch number %d, loss: %5.2f edit_distance: %5.2f" \
+                    % (train_ds.epochs_completed, train_ds.index_in_epoch,
+                       loss_val, error_val))
                 saver.save(sess, "log/cnn/model.ckpt", i)
         saver.save(sess, "log/cnn/final.ckpt")
 
