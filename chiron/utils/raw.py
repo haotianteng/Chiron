2fa5e64084c0f1879ef3a13360591399863afef5
mbhall88 <mbhall88@gmail.com>
2018-03-06 22:10:46 +0000

    clean up

--- chiron/utils/raw.py
+++ chiron/utils/raw.py
@@ -1,10 +1,10 @@
 # Copyright 2017 The Chiron Authors. All Rights Reserved.
 #
-#This Source Code Form is subject to the terms of the Mozilla Public
-#License, v. 2.0. If a copy of the MPL was not distributed with this
-#file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-#Created on Mon Apr 10 04:16:40 2017
+# Created on Mon Apr 10 04:16:40 2017
 
 from __future__ import absolute_import
 import argparse
@@ -14,6 +14,7 @@ import sys
 from chiron.utils import labelop
 import tensorflow as tf
 import numpy as np
+
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
@@ -30,7 +31,8 @@ def extract(raw_folder=None):
     if not os.path.isdir(root_folder):
         raise IOError('Input directory does not found.')
     if output_folder is None:
-        output_folder = os.path.abspath(os.path.join(root_folder, os.pardir)) + '/raw/'
+        output_folder = os.path.abspath(
+            os.path.join(root_folder, os.pardir)) + '/raw/'
     if not os.path.isdir(output_folder):
         os.mkdir(output_folder)
 
@@ -40,10 +42,12 @@ def extract(raw_folder=None):
 
     for file_n in os.listdir(root_folder):
         if file_n.endswith('fast5'):
-            output_file = output_folder + os.path.sep + os.path.splitext(file_n)[0]
-            success, (raw_data, raw_data_array) = extract_file(root_folder + os.path.sep + file_n)
+            output_file = output_folder + os.path.sep + \
+                          os.path.splitext(file_n)[0]
+            success, (raw_data, raw_data_array) = extract_file(
+                root_folder + os.path.sep + file_n)
             if success:
-                count += 1                
+                count += 1
                 example = tf.train.Example(features=tf.train.Features(feature={
                     'raw_data': _bytes_feature(raw_data.tostring()),
                     'features': _bytes_feature(raw_data_array.tostring())}))
@@ -56,8 +60,9 @@ def extract(raw_folder=None):
 
 def extract_file(input_file):
     try:
-        (raw_data, raw_label, raw_start, raw_length) = labelop.get_label_raw(input_file, FLAGS.basecall_group,
-                                                                             FLAGS.basecall_subgroup)
+        (raw_data, raw_label, raw_start, raw_length) = labelop.get_label_raw(
+            input_file, FLAGS.basecall_group,
+            FLAGS.basecall_subgroup)
     except IOError:
         return False, (None, None)
     except:
@@ -65,7 +70,8 @@ def extract_file(input_file):
 
     raw_data_array = []
     for index, start in enumerate(raw_start):
-        raw_data_array.append([start, start + raw_length[index], str(raw_label['base'][index])])
+        raw_data_array.append(
+            [start, start + raw_length[index], str(raw_label['base'][index])])
 
     return True, (raw_data, np.array(raw_data_array, dtype='S5'))
 
@@ -77,10 +83,13 @@ def run(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Transfer fast5 to raw_pair file.')
-    parser.add_argument('-i', '--input', help="Directory that store the fast5 files.")
+    parser = argparse.ArgumentParser(
+        description='Transfer fast5 to raw_pair file.')
+    parser.add_argument('-i', '--input',
+                        help="Directory that store the fast5 files.")
     parser.add_argument('-o', '--output', default=None, help="Output folder")
-    parser.add_argument('-f', '--tffile', default="train.tfrecords", help="tfrecord file")
+    parser.add_argument('-f', '--tffile', default="train.tfrecords",
+                        help="tfrecord file")
     parser.add_argument('--basecall_group', default='Basecall_1D_000',
                         help='Basecall group Nanoraw resquiggle into. Default is Basecall_1D_000')
     parser.add_argument('--basecall_subgroup', default='BaseCalled_template',
