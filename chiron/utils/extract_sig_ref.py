cc90cfc15715f842b98016eab40f66feff8c0e14
Haotian Teng <havens.teng@gmail.com>
2018-11-25 14:58:19 +0800

    add logging information

--- chiron/utils/extract_sig_ref.py
+++ chiron/utils/extract_sig_ref.py
@@ -25,15 +25,23 @@ def extract(FLAGS):
     if out_folder is None:
         raw_folder = os.path.abspath(os.path.join(out_folder, 'raw'))
         ref_folder = os.path.abspath(os.path.join(out_folder, 'reference'))
+        log_folder = os.path.abspath(os.path.join(out_folder, 'log'))
     else:
         if not os.path.isdir(FLAGS.output_dir):
             os.makedirs(FLAGS.output_dir)
         raw_folder = os.path.abspath(os.path.join(out_folder, 'raw'))
         ref_folder = os.path.abspath(os.path.join(out_folder, 'reference'))
+        log_folder = os.path.abspath(os.path.join(out_folder, 'log'))
     if not os.path.isdir(raw_folder):
         os.mkdir(raw_folder)
     if not os.path.isdir(ref_folder):
         os.mkdir(ref_folder)
+    if not os.path.isdir(log_folder):
+        os.mkdir(log_folder)
+    logging.basicConfig(filename=os.path.join(log_folder,'extract.log'),
+                        level=logging.DEBUG,
+                        filemode = 'w',
+                        format='%(asctime)s %(message)s')
     if FLAGS.recursive:
         dir_list = os.walk(root_folder)
     else:
@@ -53,12 +61,13 @@ def extract(FLAGS):
             if file_n.endswith('fast5'):
                 try:
                     raw_signal, reference = extract_file(full_file_n,FLAGS.mode)
-                    count += 1
+                    if raw_signal is None:
+                        raise ValueError("Fail in extracting raw signal.")
                     if len(raw_signal) == 0:
-                        raise ValueError("Failed in extracting " + (
-                            os.path.join(raw_folder, os.path.splitext(file_n)[0] + '.signal')))
-                except:
-                    # logging.getLogger(__name__).error("Cannot extact file %s", file_n, exc_info=True)
+                        raise ValueError("Got empty raw signal")
+                    count += 1
+                except Exception as e:
+                    logging.error("Cannot extact file %s. %s"%(full_file_n,e))
                     continue
                 with open(os.path.join(raw_folder, os.path.splitext(file_n)[0] + '.signal'), 'w+') as signal_file:
                     signal_file.write(" ".join([str(val) for val in raw_signal]))
@@ -71,20 +80,23 @@ def extract(FLAGS):
 def extract_file(input_file,mode = 'dna'):
     try:
         input_data = h5py.File(input_file, 'r')
-    except IOError:
-        return False
-    except:
-        return False
+    except IOError as e:
+        logging.error(e)
+        raise IOError(e)
+    except Exception as e:
+        logging.error(e)
+        raise Exception(e)
     raw_signal = list(input_data['/Raw/Reads'].values())[0]['Signal'].value
     if mode == 'rna':
         raw_signal = raw_signal[::-1]
     try:
         reference = input_data['Analyses/Basecall_1D_000/BaseCalled_template/Fastq'].value
-        reference = '>template\n' + reference.split('\n')[1]
+        reference = '>template\n' + reference.decode('UTF-8').split('\n')[1]
     except:
         try:
             reference = input_data['Analyses/Alignment_000/Aligned_template/Fasta'].value
-        except:
+        except Exception as e:
+            logging.info('%s has no reference.'%(input_file))
             reference = ''
     return raw_signal, reference
 
@@ -99,17 +111,18 @@ if __name__ == '__main__':
                         '--output_dir', 
                         required = True,
                         help="Directory that output the signal and reference sequence.")
+    parser.add_argument('-m',
+                        '--mode',
+                        required = True,
+                        help="Mode, dna or rna.")
     parser.add_argument('-r',
                         '--recursive',
                         action='store_true',
                         help="If recursively search subfolder")
-    parser.add_argument('-m',
-                        '--mode',
-                        default = 'rna',
-                        help="Output mode, can be chosen from dna or rna.")
     parser.add_argument('--test_number',
                         default = None,
                         type = int,
                         help="Extract test_number reads, default is None, extract all reads.")
     args = parser.parse_args(sys.argv[1:])
+
     extract(args)
