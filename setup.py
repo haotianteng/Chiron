4eff1e833ffbde4118ab7a4f318f6983198bad30
haotianteng <havens.teng@gmail.com>
2020-01-09 02:23:47 -0500

    fix twine upload and long description

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 import os
 # read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open('README.md') as f:
     long_description = f.read()
+print(long_description)
 
 print("""
 *******************************************************************
@@ -47,5 +46,6 @@ setup(
   extras_require=extras_require,
   entry_points={'console_scripts':['chiron=chiron.entry:main'],},
   long_description=long_description,
-  long_description_content_type='text/markdown'
+
+  long_description_content_type='text/markdown',
 )
